08693360c7b3eeedbf9ee26ccb4921f6eb7d2f67
Romu9319 <danielerm1510@gmail.com>
2023-10-08 21:06:57 +0200

    static and template files add

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Category, Product
 # Register your models here.
+
+admin.site.register(Category)
+#admin.site.register(Product )
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "category")
+    list_editable = ("name", "price")
